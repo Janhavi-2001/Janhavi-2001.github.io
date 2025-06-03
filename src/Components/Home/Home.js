928ff02f99a1b3362c8b07f3d4dc501349bcc02c
Janhavi-2001 <jpimplikar26@gmail.com>
2025-06-03 22:05:39 +0530

    Add resume and contact buttons to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,7 +9,9 @@ const Home = () => {
         <div className="home-container">
             <div className="home-content">
                 <TypeAnimation />
-                <p>I'm Janhavi Pimplikar, a passionate software engineer cum language enthusiast.</p>
+                <p>I'm Janhavi Pimplikar, an aspiring software engineer cum language enthusiast.</p>
+                <a className="resume-button" href="public/Documents/Janhavi_Pimplikar_Resume.pdf" download> Here's my resume! </a>
+                <a className="contact-button" href="#contact"> Get in touch! </a>
             </div>
             <Image />
         </div>
