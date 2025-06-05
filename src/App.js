4bfa3f33f7dac5c59d029084128dbb3a9f4872ee
Janhavi-2001 <jpimplikar26@gmail.com>
2025-06-05 18:03:52 +0530

    Add page loader component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
-import React from 'react';
 import Timeline from './Components/Timeline/Timeline';
 import events from './Data/events.json';
 import About from './Components/About/About';
 import ProjCards from './Components/ProjCards/ProjCards';
-
+import Loader from './Components/Loader/Loader';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <Navigation />
-      <Home />
-      <About />
-      <Timeline events={events} />
-      <ProjCards />
-      <Footer />
-    </div>
+    loading ? (
+      <Loader />
+    ) : (
+      <div className="App">
+        <Navigation />
+        <div className="slide-in">
+          <Home />
+          <About />
+          <Timeline events={events} />
+          <ProjCards />
+        </div>
+        <Footer />
+      </div>
+    )
   );
 }
 
